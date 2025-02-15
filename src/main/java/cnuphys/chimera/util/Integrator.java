c369d27ab6e99449c434e5effcf4fd73c5d64175
heddle <David.Heddle@cnu.edu>
2025-02-15 15:29:48 -0500

    first cut at area

--- src/main/java/cnuphys/chimera/util/Integrator.java
+++ src/main/java/cnuphys/chimera/util/Integrator.java
@@ -6,6 +6,34 @@ import org.apache.commons.math3.analysis.UnivariateFunction;
 public class Integrator {
 	
 	
+	/**
+	 * Evaluate the area integral of the function r^2 sin(theta) dtheta dphi over the region
+	 * @param R the radius of the sphere
+	 * @param phi0 the initial phi value of the general curve
+	 * @param phi1 the final phi value of the general curve
+	 * @param either xo or yo, the initial value of the general curve for
+	 * the case of x normal and y normal respectively
+	 * @return the contribution to the area integral
+	 */
+	public static double areaInegral(final double R, final double phi0, final double phi1, final double v0) {
+		
+		final double rr = Math.hypot(phi1, v0);
+		
+	     // Define the function to integrate
+        UnivariateFunction integrand = new UnivariateFunction() {
+            @Override
+            public double value(double t) {
+                double phi_t = (1 - t) * phi0 + t * phi1;
+                double dcosTheta_dt = (rr / R) * Math.sin(phi_t) * (phi1 - phi0);
+                return phi_t * dcosTheta_dt;
+            }
+        };
+        
+        // Use Romberg integration to evaluate the integral
+        RombergIntegrator integrator = new RombergIntegrator();
+        return R * R * integrator.integrate(1000, integrand, 0, 1);
+	}
+	
 	
 	
     public static void main(String[] args) {
